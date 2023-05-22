b20d113484d139d9efc36707c50a404270bce179
MSI\arthu <arthurgan29@gmail.com>
2023-05-21 19:16:59 -0700

    tweaks to prompt

--- HackDavis2023/Assets/Scripts/InteractionSystem/CatState.cs
+++ HackDavis2023/Assets/Scripts/InteractionSystem/CatState.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
@@ -22,9 +23,13 @@ public class CatState
 
     public void AddConversationMessage(ConversationInstance.Role role, string text)
     {
+        text = Regex.Replace(text, @"\(.*\)", "");
+        text = Regex.Replace(text, @"\s+", " ");
+        
         string append = $"{Environment.NewLine}" +
                         $"{(role == ConversationInstance.Role.User ? "Friend:" : "You:")}" +
                         $"{text}";
+        
 
         currentConversation.Append(append);
     }
@@ -60,11 +65,11 @@ public class CatState
 
         return $"{initialPrompt}{Environment.NewLine}" +
                //$"The timestamped conversation so far has been:{currentConversation.ToString()}" +
-               $"The conversation so far has been:{currentConversation.ToString()}" +
+               $"The conversation so far has been:{currentConversation.ToString()} {Environment.NewLine}" +
+               $"{Environment.NewLine}IMPORTANT: First give your verbal only response to the conversation, with no narration. Try to keep the conversation going" +
                $"You both are currently at {currentLocation.name}:{currentLocation.Description}." +
                $"You can move any of the following locations{Environment.NewLine}" +
                $"{locationPrompt}" + 
-               $"{Environment.NewLine}IMPORTANT: First give your verbal only response. Try to keep the conversation going" +
-               $". If you want to move locations, write only the location name surrounded by parenthesis on a new line. You move at most once.";
+               $". IMPORTANT: If you want to move locations based on your verbal response to the conversation, write only the location name surrounded by parenthesis on a new line. You move at most once.";
     }
 }
